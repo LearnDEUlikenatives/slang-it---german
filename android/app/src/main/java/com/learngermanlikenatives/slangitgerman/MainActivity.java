763f35e822f4fa927209cf97adb5f62b1bf87f09
Your Name <learngermanlikenatives247@gmail.com>
2026-08-20 13:48:21 +0800

    Include updated build files and sync changes

---
+++ android/app/src/main/java/com/learngermanlikenatives/slangitgerman/MainActivity.java
@@ -0,0 +1,5 @@
+package com.learngermanlikenatives.slangitgerman;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.learngermanlikenatives.slangitgerman',
+  appName: 'Slang It - German',
+  webDir: 'dist'
+};
+
+export default config;
